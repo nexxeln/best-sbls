4c23496427d2a2c2238a68ff9eb35f9b2f8e27db
Shoubhit Dash <shoubhit2005@gmail.com>
2022-06-07 14:48:29 +0530

    don't return

--- src/components/Vote/VoteImageGrid.tsx
+++ src/components/Vote/VoteImageGrid.tsx
@@ -12,7 +12,6 @@ const VoteImageGrid = () => {
 
   if (createVote.status === "loading") {
     notify();
-    return <Toaster />;
   }
 
   if (createVote.status === "success") {
@@ -20,47 +19,50 @@ const VoteImageGrid = () => {
   }
 
   return (
-    <div className="grid grid-flow-row md:grid-flow-col">
-      <div onClick={() => createVote.mutate({ choice: 1 })}>
-        <VoteImage
-          src="/images/part-1.png"
-          alt="part-1"
-          slug="Some Boring, Love Stories"
-        />
-      </div>
+    <>
+      <div className="grid grid-flow-row md:grid-flow-col">
+        <div onClick={() => createVote.mutate({ choice: 1 })}>
+          <VoteImage
+            src="/images/part-1.png"
+            alt="part-1"
+            slug="Some Boring, Love Stories"
+          />
+        </div>
 
-      <div onClick={() => createVote.mutate({ choice: 2 })}>
-        <VoteImage
-          src="/images/part-2.png"
-          alt="part-2"
-          slug="Some Boring, Love Stories, Pt.2"
-        />
-      </div>
+        <div onClick={() => createVote.mutate({ choice: 2 })}>
+          <VoteImage
+            src="/images/part-2.png"
+            alt="part-2"
+            slug="Some Boring, Love Stories, Pt.2"
+          />
+        </div>
 
-      <div onClick={() => createVote.mutate({ choice: 3 })}>
-        <VoteImage
-          src="/images/part-3.png"
-          alt="part-3"
-          slug="Some Boring, Love Stories, Pt.3"
-        />
-      </div>
+        <div onClick={() => createVote.mutate({ choice: 3 })}>
+          <VoteImage
+            src="/images/part-3.png"
+            alt="part-3"
+            slug="Some Boring, Love Stories, Pt.3"
+          />
+        </div>
 
-      <div onClick={() => createVote.mutate({ choice: 4 })}>
-        <VoteImage
-          src="/images/part-4.png"
-          alt="part-4"
-          slug="some boring love stories, pt.4"
-        />
-      </div>
+        <div onClick={() => createVote.mutate({ choice: 4 })}>
+          <VoteImage
+            src="/images/part-4.png"
+            alt="part-4"
+            slug="some boring love stories, pt.4"
+          />
+        </div>
 
-      <div onClick={() => createVote.mutate({ choice: 5 })}>
-        <VoteImage
-          src="/images/part-5.png"
-          alt="part-5"
-          slug="some boring love stories pt 5"
-        />
+        <div onClick={() => createVote.mutate({ choice: 5 })}>
+          <VoteImage
+            src="/images/part-5.png"
+            alt="part-5"
+            slug="some boring love stories pt 5"
+          />
+        </div>
       </div>
-    </div>
+      <Toaster />;
+    </>
   );
 };
 

aefee17e1367e6a678cf0d7e76d16def352c5722
Shoubhit Dash <shoubhit2005@gmail.com>
2022-06-07 13:29:46 +0530

    export type to make it reusable

--- src/components/VoteImage.tsx
+++ src/components/VoteImage.tsx
@@ -1,22 +1,8 @@
 import Image from "next/image";
 
-type VoteImageProps = {
-  src:
-    | "/images/part-1.png"
-    | "/images/part-2.png"
-    | "/images/part-3.png"
-    | "/images/part-4.png"
-    | "/images/part-5.png";
-  alt: "part-1" | "part-2" | "part-3" | "part-4" | "part-5";
-  slug:
-    | "Some Boring, Love Stories"
-    | "Some Boring, Love Stories, Pt.2"
-    | "Some Boring, Love Stories, Pt.3"
-    | "some boring love stories, pt.4"
-    | "some boring love stories pt 5";
-};
+import { ImageAndSlug } from "../types/types";
 
-const VoteImage: React.FC<VoteImageProps> = ({ src, alt, slug }) => {
+const VoteImage: React.FC<ImageAndSlug> = ({ src, alt, slug }) => {
   return (
     <div className="flex flex-col p-3 transition-all duration-200 cursor-pointer hover:scale-105">
       <Image src={src} alt={alt} width={250} height={250} />
---
+++ src/types/types.ts
@@ -0,0 +1,15 @@
+export type ImageAndSlug = {
+  src:
+    | "/images/part-1.png"
+    | "/images/part-2.png"
+    | "/images/part-3.png"
+    | "/images/part-4.png"
+    | "/images/part-5.png";
+  alt: "part-1" | "part-2" | "part-3" | "part-4" | "part-5";
+  slug:
+    | "Some Boring, Love Stories"
+    | "Some Boring, Love Stories, Pt.2"
+    | "Some Boring, Love Stories, Pt.3"
+    | "some boring love stories, pt.4"
+    | "some boring love stories pt 5";
+};
